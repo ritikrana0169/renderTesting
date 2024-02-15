71027b5bf749d1a3939b9508ca30682ba6f32fb4
Ritik rana <99540875+ritikrana0169@users.noreply.github.com>
2024-02-15 10:01:24 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,33 +1,28 @@
-from urllib.parse import quote
 from flask import Flask
-from app.models.model import db
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
 CORS(app, origins="*")
-from flask_bcrypt import Bcrypt
 
-# from flask_cors import CORS
-
-# app = Flask(__name__)
-
+# Initialize SQLAlchemy and Bcrypt
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 CORS(app, origins='*')  # for ngrok integration
-# CORS(app)
 
-bcrypt = Bcrypt(app)  # bcrypt for hashing the password
-
-# Set sql password
-sql_password = quote('root')
+# Set PostgreSQL credentials
+db_username = 'db_0vxj_user'
+db_password = 'UYyB3vLeKq78q3edzk35mFLXCeZQpMPU'
+db_host = 'localhost'  # Render usually provides a connection via localhost
+db_port = '5432'
+db_name = 'db_0vxj'
 
 # Configure the database connection
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://root:{sql_password}@localhost/db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize the database
 db.init_app(app)
-
-# Create tables if they don't exist
-with app.app_context():
-    db.create_all()
